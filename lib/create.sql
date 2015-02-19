48c5c83d2acbfe9d2b9ef1f27bdfe9320eafee02
deniznida <denizunat@hotmail.com>
2015-02-18 21:01:39 -0500

    done

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,22 @@
 -- your code here
+CREATE TABLE projects(
+  id INTEGER PRIMARY KEY AUTOINCREMENT, 
+  title text,
+  category text,
+  funding_goal text,
+  start_date date,
+  end_date date
+);
+
+CREATE TABLE users(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name text,
+  age INTEGER
+);
+
+CREATE TABLE pledges(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  amount INTEGER,
+  user_id INTEGER REFERENCES users(id),
+  project_id INTEGER REFERENCES projects(id)
+);
--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,76 @@
 -- your code here
+INSERT INTO users(name, age)
+VALUES  ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55),
+        ('John', 55);
+
+INSERT INTO projects(title, category, funding_goal, start_date, end_date)
+VALUES  ('Poject', 'some_category', 'goal', "2000-01-01", "2000-01-01"),
+        ('Poject', 'some_category', 'goal', "2000-01-01", "2000-01-01"),
+        ('Poject', 'some_category', 'goal', "2000-01-01", "2000-01-01"),
+        ('Poject', 'some_category', 'goal', "2000-01-01", "2000-01-01"),
+        ('Poject', 'some_category', 'goal', "2000-01-01", "2000-01-01"),
+        ('Poject', 'some_category', 'goal', "2000-01-01", "2000-01-01"),
+        ('Poject', 'some_category', 'goal', "2000-01-01", "2000-01-01"),
+        ('Poject', 'some_category', 'goal', "2000-01-01", "2000-01-01"),
+        ('Poject', 'some_category', 'goal', "2000-01-01", "2000-01-01"),
+        ('Poject', 'some_category', 'goal', "2000-01-01", "2000-01-01");
+
+
+
+INSERT INTO pledges (amount, user_id, project_id)
+VALUES  (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1),
+        (5, 3, 1);
+
+
+
+
+        SELECT users.name, users.age SUM(pledges.amount) 
+        FROM projects 
+        INNER JOIN users 
+        ON users.id = pledges.user_id
